2d5ad177e2b3aa3b2533a56a6b0ae70a610db9d5
Nemanja Budurin <n.budurin@gmail.com>
2021-09-02 15:33:53 +0200

    Finished info section

--- js/script.js
+++ js/script.js
@@ -17,8 +17,8 @@ $('.header__btn').on("click", function() {
 $('.features__tab').on("click", function() {
 	const clickedTab = $(this).data('tab');
 
-	$('.features__content').removeClass('features__content--active');
-	$(`.features__content[data-content="${clickedTab}"]`).addClass('features__content--active');
+	$('.features__content-text').removeClass('features__content-text--active');
+	$(`.features__content-text[data-text="${clickedTab}"]`).addClass('features__content-text--active');
 
     $('.features__tab').removeClass('features__tab--active');
     $(this).addClass('features__tab--active');
